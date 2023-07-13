2d39a6fd27d6c7d5a592ae299342113d78dbeaf0
Paschoal Colombini Neto <108839486+Pcolombini@users.noreply.github.com>
2023-07-12 23:35:10 -0300

    Add files via upload

---
+++ curso_php_udemy/6_estruturas_de_controle/1_estrutura_if/index.php
@@ -0,0 +1,13 @@
+<?php 
+
+    // condição verdadeira
+    if (5 > 2) {
+        
+        echo "Deu certo, entrou no if";
+    }
+
+    // condição falsa
+    if (5 < 2){
+        echo "Não vai imprimir, condição falsa";
+    }
+
