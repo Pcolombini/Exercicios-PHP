e970a8ab3a47a1a2e7dc15857a6e407796f22013
Paschoal <paschoal.colombini@live.com>
2023-07-15 21:03:08 -0300

    alteração para margear

--- curso_php_udemy/6_estruturas_de_controle/1_estrutura_if/index.php
+++ curso_php_udemy/6_estruturas_de_controle/1_estrutura_if/index.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php 
 
     // condição verdadeira
@@ -12,18 +11,3 @@
         echo "Não vai imprimir, condição falsa";
     }
 
-=======
-<?php 
-
-    // condição verdadeira
-    if (5 > 2) {
-        
-        echo "Deu certo, entrou no if";
-    }
-
-    // condição falsa
-    if (5 < 2){
-        echo "Não vai imprimir, condição falsa";
-    }
-
->>>>>>> 2d39a6fd27d6c7d5a592ae299342113d78dbeaf0
--- curso_php_udemy/6_estruturas_de_controle/4_estrutura_else/index.php
+++ curso_php_udemy/6_estruturas_de_controle/4_estrutura_else/index.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php 
 
 if (5 > 2)
@@ -12,19 +11,4 @@ if ("teste" === 5) {
     echo "entrou no if<br>\n";
 } else {
     echo "Entrou no else<br>\n";
-=======
-<?php 
-
-if (5 > 2)
-{
-    echo "Número 5 é maior que 2, entrou no if!<br>\n";
-} else {
-    echo "Não é maior e não entrou no else!<br>\n";
-}
-
-if ("teste" === 5) {
-    echo "entrou no if<br>\n";
-} else {
-    echo "Entrou no else<br>\n";
->>>>>>> 2d39a6fd27d6c7d5a592ae299342113d78dbeaf0
 }
