8b6f48bda31ff31076f307185c181d5155d2f6d4
Paschoal Colombini Neto <108839486+Pcolombini@users.noreply.github.com>
2023-08-26 17:57:04 -0300

    add aula com exercicio array e uso do for

---
+++ curso_php_udemy/7_estruturas_de_repetição/10_exercicio_30/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="favicon-16x16.png" type="image/x-icon">
+    <title>Exercício 30</title>
+</head>
+<body>
+    <h1 id="titulo">Exercício 30</h1>
+    <hr>
+    <p>
+        <ul class="list">
+            <li>Crie um array com número de 1 a 20;</li>
+            <li>Crie um loop for para este array;</li>
+            <li>Imprima apenas os pares;</li>
+        </ul>
+    </p>
+    <hr>
+    <div id="box">
+        <?php 
+            $arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+
+            for ($i=0; $i < count($arr); $i++) { 
+
+                if ($arr[$i] % 2 == 0) {
+                    echo "Número: <b>$arr[$i]</b><br>";
+                }
+                
+            }
+            
+            // for ($i=1; $i < count($arr) ; $i+=2) { 
+            //     echo "Número: $arr[$i]<br>";
+            // }
+        ?>
+    </div>
+    <script>
+        let titulo = document.getElementById('titulo');
+        let box = document.getElementById('box');
+        titulo.style.textAlign = 'center';
+        box.style.border = 'solid 1px black';
+        box.style.fontSize = '16pt';
+        box.style.width = '250px';
+        box.style.margin = 'auto';
+        box.style.textAlign = 'center';
+    </script>
+</body>
+</html>
