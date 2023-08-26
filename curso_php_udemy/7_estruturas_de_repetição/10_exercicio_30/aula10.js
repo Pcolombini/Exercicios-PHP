6d780c2ae71f2178c153d3b6e2b95867569d3aa4
Paschoal Colombini Neto <108839486+Pcolombini@users.noreply.github.com>
2023-08-26 18:05:41 -0300

    add file js aula 10

---
+++ curso_php_udemy/7_estruturas_de_repetição/10_exercicio_30/aula10.js
@@ -0,0 +1,8 @@
+let titulo = document.getElementById('titulo');
+let box = document.getElementById('box');
+titulo.style.textAlign = 'center';
+box.style.border = 'solid 1px black';
+box.style.fontSize = '16pt';
+box.style.width = '250px';
+box.style.margin = 'auto';
+box.style.textAlign = 'center';
--- curso_php_udemy/7_estruturas_de_repetição/10_exercicio_30/index.php
+++ curso_php_udemy/7_estruturas_de_repetição/10_exercicio_30/index.php
@@ -26,7 +26,7 @@
                 if ($arr[$i] % 2 == 0) {
                     echo "Número: <b>$arr[$i]</b><br>";
                 }
-                
+
             }
             
             // for ($i=1; $i < count($arr) ; $i+=2) { 
@@ -34,15 +34,6 @@
             // }
         ?>
     </div>
-    <script>
-        let titulo = document.getElementById('titulo');
-        let box = document.getElementById('box');
-        titulo.style.textAlign = 'center';
-        box.style.border = 'solid 1px black';
-        box.style.fontSize = '16pt';
-        box.style.width = '250px';
-        box.style.margin = 'auto';
-        box.style.textAlign = 'center';
-    </script>
+    <script src="aula10.js"></script>
 </body>
 </html>
