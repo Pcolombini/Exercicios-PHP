dbcf9a7d79af8f7fb25460487f0ca8c7f0241a69
Paschoal Colombini Neto <108839486+Pcolombini@users.noreply.github.com>
2023-08-26 16:55:39 -0300

    add aula 8 do while

---
+++ curso_php_udemy/7_estruturas_de_repetição/8_do_while/index.php
@@ -0,0 +1,26 @@
+<center>
+    <div>
+        <p style="font-size: 22pt;">
+            <strong>
+                <?php   
+
+                    $a = 0;
+
+                    echo "$a <br>";
+                     
+                    do {
+                        $a++;
+                        echo "$a <br>";
+                    } while ($a < 10);
+                   
+                    $i = 100;
+
+                    do {
+                        echo "Decrementando i = $i<br>";
+                        $i--;
+                    } while ($i >= 0);
+                ?>
+            </strong>
+        </p>
+    </div>
+</center>
