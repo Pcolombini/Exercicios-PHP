6a852ccaa41a8ac09b5d308a466ed5bf2e2a2cc1
Paschoal Colombini Neto <108839486+Pcolombini@users.noreply.github.com>
2023-08-26 21:09:13 -0300

    add aula 4 short tags e config no php.ini para habilitar
    abrir o php.ini, pesquisar por short_open, definir como on, e reiniciar o servidor

---
+++ curso_php_udemy/8_inclusão_de_codigo/4_short_tags/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Short Tags</title>
+</head>
+<body>
+	<div>
+		<?php 
+			echo "Testando código php nas tags normais!";
+		?>
+	</div>
+	<div>
+		<?
+			echo "Testado as shorts tags";
+		?>
+	</div>
+
+</body>
+</html>
---
+++ curso_php_udemy/8_inclusão_de_codigo/4_short_tags/shortTags.md
@@ -0,0 +1,10 @@
+# shortTags
+
+- A **Short tag** é uma funcionalidade para adicionar código PHP em uma página;
+- Este recurso **depende de uma configuração do servidor** para funcionar;
+- Por isso **é desencorajado seu uso**, pode ser que o código não funcione;
+- Exemplo:
+
+```
+	<?echo "teste";?>
+```
