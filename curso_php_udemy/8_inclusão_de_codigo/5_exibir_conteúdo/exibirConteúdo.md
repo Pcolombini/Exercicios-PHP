b72ff2bf81f592f6e099d76b7f066809ae04bd48
Paschoal Colombini Neto <108839486+Pcolombini@users.noreply.github.com>
2023-08-26 21:20:36 -0300

    add aula 5 exibir conteudo na pag html

---
+++ curso_php_udemy/8_inclusão_de_codigo/5_exibir_conteúdo/exibirConteúdo.md
@@ -0,0 +1,9 @@
+# Exibição de conteúdo
+
+- com uma **técnica semelhante ao short tags**, podemos exibir conteúdo sem echo;
+- Ótima estrategia para resumir as chamadas PHP **apenas para exibição de valores**;
+- Exemplo:
+
+```php
+<?= 'teste';?>
+```
---
+++ curso_php_udemy/8_inclusão_de_codigo/5_exibir_conteúdo/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Exibição de conteúdo</title>
+</head>
+<body>
+	<?php 
+		$name = 'Paschoal';
+	 ?>
+	<form action="">
+		<div>
+			<input type="text" name="" value="" placeholder="<?= $name;?>">
+		</div>
+		<div>
+			<input type="submit" name="" value="Enviar">
+		</div>
+	</form>
+</body>
+</html>
