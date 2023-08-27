d5834f8627a44c97813e1abcb3f0cc2a82eb1d66
Paschoal Colombini Neto <108839486+Pcolombini@users.noreply.github.com>
2023-08-26 22:32:03 -0300

    add aula 2 chamando

---
+++ curso_php_udemy/9_funcoes/2_chamando_funcao/index.php
@@ -0,0 +1,18 @@
+<?php 
+	
+	function soma() 
+	{
+		$a = 5;
+		$b = 7;
+		echo $a + $b;
+		echo "<br>";
+	}
+
+	// CHAMANDO / INVOCANDO
+	soma();
+	soma();
+
+	// CHAMANDO DO PHP
+	
+	echo strtoupper("transfou tudo em maiúsculo");
+ ?>
