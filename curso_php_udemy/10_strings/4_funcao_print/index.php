9fe543c9a97a8ddf1d013855c9418e2b5503b681
Paschoal Colombini Neto <108839486+Pcolombini@users.noreply.github.com>
2023-10-02 16:12:43 -0300

    aula 4 função print

---
+++ curso_php_udemy/10_strings/4_funcao_print/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Função Print</title>
+    <style>
+        span {
+            color: blue;
+        }
+    </style>
+</head>
+<body>
+    <header>
+        <h1>Função Print</h1>
+    </header>
+    <main>
+        <section class="conteudo">
+            <ul>
+                <li>A função <span><strong>print</strong></span> tem a funcionalidade de echo;</li>
+                <li>Pode <span><strong>imprimir uma string que foi passada como argumento;</strong></span></li>
+                <p>
+                    Exemplo:<br>
+                    <code>
+                        print("Testando");
+                    </code>
+                </p>
+            </ul>
+        </section>
+        <hr>
+        <section class="codigo">
+            <p> 
+                <pre>
+                    <code>
+                    print("imprimindo algo com print()");
+                    </code>
+                </pre>
+                Saída:<br>
+                <center><span style="color: black;" ><strong>imprimindo algo com print()</strong></span></center>
+            </p>
+            <?php 
+
+            ?>
+        </section>
+    </main>
+</body>
+</html>
